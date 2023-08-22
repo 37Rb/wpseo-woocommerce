512482a3b4a52b1d6311d97580be118cabe4497b
Vraja Das <vraja.pro@gmail.com>
2023-08-22 15:55:14 +0200

    Improve phpcs ignore comment

--- classes/woocommerce-seo.php
+++ classes/woocommerce-seo.php
@@ -442,7 +442,7 @@ class Yoast_WooCommerce_SEO {
 	public function config_page_styles() {
 		global $pagenow;
 
-		// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Nonce is not required here.
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Nonce is not required here because we are purely doing a strict equals check.
 		$is_get_wpseo_woo = ( isset( $_GET['page'] ) && is_string( $_GET['page'] ) && $_GET['page'] === 'wpseo_woo' );
 
 		$is_wpseo_woocommerce_page = ( $pagenow === 'admin.php' && $is_get_wpseo_woo );
